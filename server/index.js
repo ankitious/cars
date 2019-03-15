99bb49e407fb161002b2035249f966384f91755c
ankitious <ankitpanchal0404@gmail.com>
2019-03-15 22:48:41 +0530

    #9 fixed some issues

--- server/index.js
+++ server/index.js
@@ -13,10 +13,12 @@ app.use(express.static(__dirname + "/public"));
 
 routes(app);
 
-app.use(express.static(path.join(__dirname, '../client/build')));
-app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
-});
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+    app.get('*', function (req, res) {
+        res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+    });
+}
 
 
 app.listen(port, function() {
